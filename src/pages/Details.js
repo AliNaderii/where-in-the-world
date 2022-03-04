d3493d50cb0e7ab92c9c9a3c1e0c7d18efaa9a92
iAmAli <ali.naderi72@gmail.com>
2022-03-04 17:27:31 +0330

    fixed the name of some countries

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -13,7 +13,7 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Details() {
   const name = useParams().country;
-  const correctName = name.replace(' ', '%20');
+  const correctName = name.replace(/\s/g, '%20');
   const url = `https://restcountries.com/v2/name/${correctName}`;
   const { data, isPending } = useAxios(url);
   // MUST WAIT TILL THE PROMIS RESOLVES THEN DESTRUCTURE THE DATA
